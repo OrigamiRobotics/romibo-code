cc3f106495dfc26c4f1016d7bfd37d3e79afc647
Mansfield Mark <mmark@andrew.cmu.edu>
2013-02-06 18:50:06 -0500

    Fixed pin mappings again for L/R, and testing analogWrite

--- Arduino/libraries/RomiboHardware/RomiboPinMappings.h
+++ Arduino/libraries/RomiboHardware/RomiboPinMappings.h
@@ -127,13 +127,13 @@
 	enum { ROMIBO_LEFT_MOTOR = 0, ROMIBO_RIGHT_MOTOR = 1 };
 // Updated Pin Numbers 02/06/2013 - Mansfield Mark
 // Pin Configuration:
-// Left: 7,8
 // Right: 2,3
+// Left: 7,8
 // Unsure if this is truly Pins 1/2 in the wiring
 // OR if it is reversed to adjust for the motor inversion
 //
-	const int mot_drv1_pin[2] = { 7,2 };   // DRV8833 inputs IN1 *** Caz *** these are now the correct pin numbers
-	const int mot_drv2_pin[2] = { 8,3 };   // DRV8833 inputs IN2 *** Caz *** these are now the correct pin numbers
+	const int mot_drv1_pin[2] = { 2,7 };   // DRV8833 inputs IN1 *** Caz *** these are now the correct pin numbers
+	const int mot_drv2_pin[2] = { 3,8 };   // DRV8833 inputs IN2 *** Caz *** these are now the correct pin numbers
 	const int motor_sleep_pin = 5;		// DRV8833 sleep pin ( active low = sleep ) // *** Caz *** new pin that *must* be handled
 	const int motor_fault_pin = 6;		// DRV8833 driver fault pin. No code yet. We should write some 
 	
